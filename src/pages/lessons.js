9ec5b04ee5ff63c74e04bf3feecba7ae37cf8419
Kevin Boucinha <kevinboucinha@gmail.com>
2022-06-27 20:16:29 -0300

    add layout component

---
+++ src/pages/lessons.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function Lessons() {
+    return (
+        <Layout>
+            <div>A duda é maravilhosa</div>
+        </Layout>
+    );
+}
