0e7af2165e5810d377e1538d014a625b5cc99144
Arthur Lamberti <arthurlamberti@MacBook-Pro-de-Arthur.local>
2022-06-29 20:41:00 -0300

    adicionado modal na tela de disciplinas

--- src/pages/disciplines.js
+++ src/pages/disciplines.js
@@ -5,87 +5,143 @@ import Grid from "@mui/material/Grid";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CustomBox from "../components/CustomBox";
 import CustomLine from "../components/CustomLine";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { TextField } from "@mui/material";
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const theme = createTheme();
 
-export default function Disciplines() {
- 
-  let disciplina = 
-    [
-      {
-        "id":"1",
-        "codigo":"1",
-        "creditos":4,
-        "nome":"Alest 1",
-        "objetivo":"algum objetivo",
-        "ementa":"lorem ipsum",
-        "nivel":3,
-        "ativa":true
-      },
-      {
-        "id":"2",
-        "codigo":"2",
-        "creditos":4,
-        "nome":"Alest 2",
-        "objetivo":"algum objetivo",
-        "ementa":"lorem ipsum",
-        "nivel":3,
-        "ativa":true
-      },
-      {
-        "id":"3",
-        "codigo":"3",
-        "creditos":4,
-        "nome":"Alest 3",
-        "objetivo":"algum objetivo",
-        "ementa":"lorem ipsum",
-        "nivel":3,
-        "ativa":true
-      },
-      {
-        "id":"4",
-        "codigo":"4",
-        "creditos":4,
-        "nome":"Alest 4",
-        "objetivo":"algum objetivo",
-        "ementa":"lorem ipsum",
-        "nivel":3,
-        "ativa":true
-      }
-  
+let disciplinaMock =
+  [
+    {
+      "id": "1",
+      "codigo": "41231",
+      "creditos": 4,
+      "nome": "Alest 1",
+      "objetivo": "algum objetivo",
+      "ementa": "lorem ipsum",
+      "nivel": 3,
+      "ativa": true
+    },
+    {
+      "id": "2",
+      "codigo": "21231",
+      "creditos": 4,
+      "nome": "Alest 2",
+      "objetivo": "algum objetivo",
+      "ementa": "lorem ipsum",
+      "nivel": 3,
+      "ativa": true
+    },
+    {
+      "id": "3",
+      "codigo": "31234",
+      "creditos": 4,
+      "nome": "Alest 3",
+      "objetivo": "algum objetivo",
+      "ementa": "lorem ipsum",
+      "nivel": 3,
+      "ativa": true
+    },
+    {
+      "id": "4",
+      "codigo": "44231",
+      "creditos": 4,
+      "nome": "Alest 4",
+      "objetivo": "algum objetivo",
+      "ementa": "lorem ipsum",
+      "nivel": 3,
+      "ativa": true
+    }
+
   ]
 
-  function adicionarCurso() {
+export default function Disciplines() {
+  const [disciplina, setDisciplina] = React.useState(disciplinaMock)
+
+  const [showMatricula, setShowMatricula] = React.useState(false);
+  const handleShowMatriculaOpen = () => setShowMatricula(true);
+  const handleShowMatriculaClose = () => setShowMatricula(false);
+
+  const [addDisciplina, setAddDisciplina] = React.useState(false)
+  const handleAddDisciplinaOpen = () => setAddDisciplina(true)
+  const handleAddDisciplinaClose = () => setAddDisciplina(false)
+
+
+
+
+  function adicionarDisciplina() {
     //tem que usar state pra atualizar o front
-    disciplina.push({
-      "id":"5",
-      "nome":"Outro curso",
+    const novaDisciplina = {
+      "id": "5",
+      "nome": "Outro curso",
       "curriculos": [
         {
-          "id":"999",
-          "nome":"curriculo 1"
+          "id": "999",
+          "nome": "curriculo 1"
         }
       ]
-    })
+    }
+
+    setDisciplina([...disciplina,novaDisciplina])
+    setAddDisciplina(false)
+    //limpar campos da disciplina
   }
 
   return <>
-      <ThemeProvider theme={theme}>
-        <Container component="main">
-        <Button onClick={adicionarCurso}>Cadastrar Disciplina</Button>
+    <ThemeProvider theme={theme}>
+      <Container component="main">
+        <Button onClick={handleAddDisciplinaOpen}>Cadastrar Disciplina</Button>
 
         <Grid container>
-            <Grid item > 
-              <CustomLine title="Disciplinas" curriculo="Curriculos" cursos="Cursos" />
-            </Grid>
+          <Grid item >
+            <CustomLine title="Disciplinas" curriculo="Curriculos" cursos="Cursos" />
+          </Grid>
           {
-            disciplina.map(function(disciplina, index){
+            disciplina.map(function (disciplina, index) {
               return (
                 <>
-                <Grid item key={index}> 
-                  <CustomLine onClick={()=>alert(disciplina.nome)} title={disciplina.nome} curriculo="Algum curriculo ai" cursos="ES"/>
-                </Grid>
+                  <Grid item key={disciplina.id}>
+                    <CustomLine onClick={handleShowMatriculaOpen}
+                      title={disciplina.nome}
+                      curriculo="Algum curriculo ai"
+                      cursos="ES" />
+
+                  </Grid>
+                  <Modal
+                    open={showMatricula}
+                    onClose={handleShowMatriculaClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography id="modal-modal-title" variant="h3" component="h2">
+                        {disciplina.nome}
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        {disciplina.codigo}-{disciplina.creditos}
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        objetivo da disciplina: {disciplina.objetivo}
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        ementa da disciplina: {disciplina.ementa}
+                      </Typography>
+                    </Box>
+                  </Modal>
                 </>
               )
             })
@@ -93,8 +149,33 @@ export default function Disciplines() {
 
         </Grid>
 
-        </Container>
+        <Modal
+                open={addDisciplina}
+                onClose={handleAddDisciplinaClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography id="modal-modal-title" variant="h5" component="h2">
+                   Adicionar uma disciplina
+                  </Typography>
+                  
+                  <TextField id="standard-basic" label="Nome da disciplina" variant="standard" />
+                  <TextField id="standard-basic" label="Codigo da disciplina" variant="standard" />
+                  <TextField id="standard-basic" label="Creditos da disciplina" variant="standard" />
+                  <TextField id="standard-basic" label="Objetivo da disciplina" variant="standard" />
+                  <TextField id="standard-basic" label="Ementa da disciplina" variant="standard" />
+                  <TextField id="standard-basic" label="Nivel da disciplina" variant="standard" />
+                  {/* codigo, creditos, nome, objetivo, ementa, nivel, ativa */}
 
-      </ThemeProvider> 
+                  <Button onClick={adicionarDisciplina}>Cadastrar Disciplina</Button>
+                </Box>
+                
+              </Modal>
+      </Container>
+
+     
+
+    </ThemeProvider>
   </>
 }
