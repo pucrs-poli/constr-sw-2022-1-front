7ae37dd3c84684fbb857772954b5d30f4736954e
Bruno F Marcelino <bruno.marcelino@acad.pucrs.br>
2022-07-04 16:40:50 -0300

    botao de adicionar curso/curriculo

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -4,54 +4,79 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CustomBox from "../components/CustomBox";
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { TextField } from "@mui/material";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const theme = createTheme();
 
-export default function Courses() {
-  // const [courses, setCourses] = React.useState("Courses")
+let courses = [
+  {
+    "id":"1",
+    "nome":"Engenharia de software",
+    "curriculos": [
+      {
+        "id":"999",
+        "nome":"curriculo 1"
+      }
+    ]
+  },{
+    "id":"2",
+    "nome":"Engenharia da computacao",
+    "curriculos": [
+      {
+        "id":"998",
+        "nome":"curriculo 1"
+      }
+    ]
+  },{
+    "id":"3",
+    "nome":"Sistemas de informacao",
+    "curriculos": [
+      {
+        "id":"999",
+        "nome":"curriculo 1"
+      }
+    ]
+  },{
+    "id":"4",
+    "nome":"Ciencia da computacao",
+    "curriculos": [
+      {
+        "id":"999",
+        "nome":"curriculo 1"
+      }
+    ]
+  },
+]
 
-  let courses = [
-    {
-      "id":"1",
-      "nome":"Engenharia de software",
-      "curriculos": [
-        {
-          "id":"999",
-          "nome":"curriculo 1"
-        }
-      ]
-    },{
-      "id":"2",
-      "nome":"Engenharia da computacao",
-      "curriculos": [
-        {
-          "id":"998",
-          "nome":"curriculo 1"
-        }
-      ]
-    },{
-      "id":"3",
-      "nome":"Sistemas de informacao",
-      "curriculos": [
-        {
-          "id":"999",
-          "nome":"curriculo 1"
-        }
-      ]
-    },{
-      "id":"4",
-      "nome":"Ciencia da computacao",
-      "curriculos": [
-        {
-          "id":"999",
-          "nome":"curriculo 1"
-        }
-      ]
-    },
-  ]
+export default function Courses() {  
+  //const [courses, setCourses] = React.useState("Courses")   
 
-  function adicionarCurso() {
+  const [curso, setCurso] = React.useState(courses)
+
+  const [addCourse, setAddCourse] = React.useState(false)
+  const handleAddCourseOpen = () => setAddCourse(true)
+  const handleAddCourseClose = () => setAddCourse(false)
+
+  const [courseName, setCourseName] = React.useState('');
+
+  function adicionarCursoOld() {
     //tem que usar state pra atualizar o front
+    console.log("passou aqui");
     courses.push({
       "id":"5",
       "nome":"Outro curso",
@@ -64,19 +89,38 @@ export default function Courses() {
     })
   }
 
+  function adicionarCurso() {
+    //tem que usar state pra atualizar o front
+    const novoCurso = {
+      "id":"5",
+      "nome": courseName,
+      "curriculos": [
+        {
+          "id":"999",
+          "nome":"curriculo 1"
+        }
+      ]
+    }
+
+    setCurso([...curso,novoCurso])
+    setAddCourse(false)
+    setCourseName("")
+    //limpar campos do curso
+  }
+
   return <>
       <ThemeProvider theme={theme}>
         <Container component="main">
-        <Button onClick={adicionarCurso}>Cadastrar curso</Button>
+        <Button onClick={handleAddCourseOpen}>Cadastrar curso</Button>
 
         <Grid container>
 
           {
-            courses.map(function(course, index){
+            curso.map(function(curso, index){
               return (
                 <>
                 <Grid item key={index}> 
-                  <CustomBox onClick={()=>alert(course.nome)} title={course.nome}/>
+                  <CustomBox onClick={()=>alert(curso.nome)} title={curso.nome}/>
                 </Grid>
                 </>
               )
@@ -84,7 +128,25 @@ export default function Courses() {
           }
 
         </Grid>
+        <Modal
+                open={addCourse}
+                onClose={handleAddCourseClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography id="modal-modal-title" variant="h5" component="h2">
+                   Adicionar um curso
+                  </Typography>
+                  
+                  <TextField id="name" name="name" label="Nome do curso" variant="standard" value={courseName}
+                   onChange={(e) => setCourseName(e.target.value)}/>
+                  {/* codigo, creditos, nome, objetivo, ementa, nivel, ativa */}
 
+                  <Button onClick={adicionarCurso}>Cadastrar Curso</Button>
+                </Box>
+                
+              </Modal>
         </Container>
 
       </ThemeProvider> 
--- src/pages/curriculums.js
+++ src/pages/curriculums.js
@@ -4,25 +4,65 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CustomBox from "../components/CustomBox";
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { TextField } from "@mui/material";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const theme = createTheme();
 
-export default function Curriculums() {
-  let curriculos = [
-    {
-      "id":"999",
-      "nome":"curriculo 1"
-    },
-    {
-      "id":"998",
-      "nome":"curriculo 2"
-    },{
-      "id":"997",
-      "nome":"curriculo 3"
-    }
-  ]
+let curriculosMock = [
+  {
+    "id":"999",
+    "nome":"curriculo 1"
+  },
+  {
+    "id":"998",
+    "nome":"curriculo 2"
+  },{
+    "id":"997",
+    "nome":"curriculo 3"
+  }
+]
 
-  function adicionarCurrilo() {
+export default function Curriculums() {
+
+  const [curriculos, setCurriculos] = React.useState(curriculosMock)
+
+  const [addCurriculums, setAddCurriculums] = React.useState(false)
+  const handleAddCurriculumsOpen = () => setAddCurriculums(true)
+  const handleAddCurriculumsClose = () => setAddCurriculums(false)
+
+  const [curriculumsName, setCurriculumsName] = React.useState('');
+  const [curriculumsId, setCurriculumsId] = React.useState('');
+
+  function adicionarCurriculo() {
+    //tem que usar state pra atualizar o front
+    const novoCurriculo = {
+      "id": curriculumsId,
+      "nome": curriculumsName    
+    }
+  
+    setCurriculos([...curriculos,novoCurriculo])
+    setAddCurriculums(false)
+    setCurriculumsName("")
+    setCurriculumsId("")
+    //limpar campos do curriculo
+  }
+
+  function adicionarCurriloOld() {
     //tem que usar state pra atualizar o front
     curriculos.push(
       {
@@ -35,7 +75,7 @@ export default function Curriculums() {
   return <>
       <ThemeProvider theme={theme}>
         <Container component="main">
-        <Button onClick={adicionarCurrilo}>Cadastrar curriculo</Button>
+        <Button onClick={handleAddCurriculumsOpen}>Cadastrar curriculo</Button>
 
         <Grid container>
 
@@ -52,6 +92,27 @@ export default function Curriculums() {
           }
 
         </Grid>
+
+        <Modal
+              open={addCurriculums}
+              onClose={handleAddCurriculumsClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h5" component="h2">
+                  Adicionar um curriculo
+                </Typography>
+                <TextField id="id" name="id" label="Id do curriculo" variant="standard" value={curriculumsId}
+                  onChange={(e) => setCurriculumsId(e.target.value)}/>
+                <TextField id="name" name="name" label="Nome do curriculo" variant="standard" value={curriculumsName}
+                  onChange={(e) => setCurriculumsName(e.target.value)}/>
+                {/* codigo, creditos, nome, objetivo, ementa, nivel, ativa */}
+
+                <Button onClick={adicionarCurriculo}>Cadastrar Curriculo</Button>
+              </Box>
+                
+        </Modal>
 
         </Container>
 
